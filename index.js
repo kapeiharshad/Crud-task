5a0fda73bb12e4e509a8d91183ecbac6643b0e99
kapeiharshad <kapeiharshad@gmail.com>
2020-10-22 00:33:18 +0530

    file routing done

--- index.js
+++ index.js
@@ -3,9 +3,29 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
+const fs = require("fs");
 
-mongoose.connect("mongodb://127.0.0.1:27017/curdtask", {useNewUrlParser: true});
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/curdtask",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  () => {
+    console.log("Mongodb Connected");
+  }
+);
+mongoose.set("useCreateIndex", true);
+
 app.use(bodyParser.json());
+
+const controllerFiles = fs.readdirSync("./controller/");
+
+controllerFiles.forEach((controllerFile) => {
+  let controller = require(`./controller/${controllerFile}`);
+  let str1 = controllerFile.split("Controller.js");
+  app.use(`/${str1[0]}`, controller);
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
